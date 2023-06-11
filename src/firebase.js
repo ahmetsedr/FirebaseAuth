dd60f9569bec70ff54f1b876fb3c7279d30ac761
ahmetsedr <115935249+ahmetsedr@users.noreply.github.com>
2023-06-11 12:32:52 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCa3GRFs6d9iSHr1EJt7S_tavNlRUKqBfQ",
-    authDomain: "chatcominity.firebaseapp.com",
-    projectId: "chatcominity",
-    storageBucket: "chatcominity.appspot.com",
-    messagingSenderId: "581424238858",
-    appId: "1:581424238858:web:5bdfd050345f94fae5eaf3",
-    databaseURL: "https://chatcominity-default-rtdb.europe-west1.firebasedatabase.app/"
+    // ...
 };
 
 const app = initializeApp(firebaseConfig);
